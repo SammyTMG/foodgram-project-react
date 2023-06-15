6431c7a8facc91f169753dbe096b90bb55a6d84f
SammyTMG <tenyakovaa@mail.ru>
2023-06-15 22:31:30 +0000

    1Review5

--- backend/users/views.py
+++ backend/users/views.py
@@ -16,6 +16,7 @@ class CustomUserViewSet(UserViewSet):
     '''Вьюсет для юзеров и подписок. '''
     serializer_class = CustomUserSerializer
     pagination_class = LimitPageNumberPagination
+    permission_classes = (IsAuthenticated, )
 
     def get_queryset(self):
         return User.objects.all()
