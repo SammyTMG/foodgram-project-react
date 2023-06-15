9399ac68fee6b69ff1b9fc33c582bb48547d8402
SammyTMG <tenyakovaa@mail.ru>
2023-06-15 11:47:58 +0000

    Review6

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -72,14 +72,14 @@ class ReadRecipeSerializer(ModelSerializer):
         user = request.user
         if not request or request.user.is_anonymous:
             return False
-        return user.favourite.filter(author=obj).exists()
+        return user.favourite.filter(id=obj.id).exists()
 
     def get_is_in_shopping_cart(self, obj):
         request = self.context.get('request')
         user = request.user
         if not request or (request.user and request.user.is_anonymous):
             return False
-        return user.shopping_cart.filter(author=obj).exists()
+        return user.shopping_cart.filter(id=obj.id).exists()
 
 
 class AddIngredientInRecipeSerializer(ModelSerializer):
