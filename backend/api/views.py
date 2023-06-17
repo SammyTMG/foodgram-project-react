c1896efd93c6def9c58791dfc5f99ce4bf27a689
SammyTMG <tenyakovaa@mail.ru>
2023-06-17 18:48:23 +0000

    Review3

--- backend/api/views.py
+++ backend/api/views.py
@@ -9,8 +9,7 @@ from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.permissions import (IsAuthenticated,
-                                        IsAuthenticatedOrReadOnly)
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
